d6928c707675213ea56cca6ea2d440b01641ec61
Chen Wei <130945184+CW-meow@users.noreply.github.com>
2023-11-23 21:51:26 +0800

    javadocs
    added java docs

--- cams/model/Camp.java
+++ cams/model/Camp.java
@@ -3,10 +3,14 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+
+/**
+ * A model object for all relevant camp information
+ */
 public class Camp implements Serializable {
     private int id;
     private String campName;
-    private ArrayList<LocalDate> dates; // 0: start, 1: end, 2: Register close
+    private ArrayList<LocalDate> dates; // 0: start, 1: end
     private LocalDate registrationClosingDate;
     private boolean visibility;
     private String userGroup;
@@ -21,6 +25,17 @@ public class Camp implements Serializable {
     private ArrayList<Integer> enquiryIDs;
     private ArrayList<Integer> suggestionIDs;
 
+
+    /**
+     * Constructor for a new camp
+     * @param campID The ID of the camp
+     * @param campName The name of the camp
+     * @param dates ArrayList<LocalDate> for the dates of the camp in the format <start, end>
+     * @param faculty The faculty/user group of the camp
+     * @param visibility Whether the camp is visible to all students
+     * @param staffInCharge The ID of the staff in charge
+     * @param registrationClosingDate The registration close date
+     */
     public Camp(int campID, String campName, ArrayList<LocalDate> dates, String faculty, boolean visibility, String staffInCharge, LocalDate registrationClosingDate) {
         // Default values
         this.id = campID;
@@ -44,7 +59,10 @@ public class Camp implements Serializable {
     }
 
     
-    // For creating a defensive copy of the Camp object
+    /**
+     * Method for creating a defensive copy of the Camp object
+     * @param other The original camp object to be copied
+     */
     public Camp(Camp other) {
         this.id = other.id;
         this.campName = other.campName;
@@ -64,115 +82,253 @@ public class Camp implements Serializable {
         this.suggestionIDs = new ArrayList<>(other.suggestionIDs);
     }
 
-    // Getters and setters for attributes
+    /**
+     * Getter for campID
+     * @return int
+     */
     public int getId() {
         return id;
     }
 
+
+    /**
+     * Setter for campID
+     * @param id The new camp ID
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+
+    /**
+     * Getter for camp name
+     * @return String of camp name
+     */
     public String getCampName() {
         return this.campName;
     }
 
+
+    /**
+     * Setter for camp name
+     * @param campName The new camp name
+     */
     public void setCampName(String campName) {
         this.campName = campName;
     }
 
+
+    /**
+     * Getter for camp start date
+     * @return LocalDate object of camp start date
+     */
     public LocalDate getStartDate() {
         return this.dates.get(0);
     }
 
+
+    /**
+     * Getter for camp end date
+     * @return LocalDate object of camp end date
+     */
     public LocalDate getEndDate() {
         return this.dates.get(1);
     }
 
+
+    /**
+     * Getter for the camp dates
+     * @return ArrayList<LocalDate> of camp dates in format <start, end>
+     */
     public ArrayList<LocalDate> getDates() {
         return this.dates;
     }
 
+
+    /**
+     * Setter for camp dates
+     * @param dates ArrayList<LocalDate> of the new camp dates
+     */
     public void setDates(ArrayList<LocalDate> dates) {
         this.dates = dates;
     }
 
+
+    /**
+     * Getter for camp registration close date
+     * @return LocalDate object of the registration close date
+     */
     public LocalDate getRegistrationClosingDate() {
         return this.registrationClosingDate;
     }
 
+
+    /**
+     * Setter for camp registration close date
+     * @param registrationClosingDate LocalDate object for the registration close date
+     */
     public void setRegistrationClosingDate(LocalDate registrationClosingDate) {
         this.registrationClosingDate = registrationClosingDate;
     }
 
+
+    /**
+     * Getter for camp visibility
+     * @return boolean of whether camp is visible
+     */
     public boolean getVisibility() {
         return this.visibility;
     }
 
+
+    /**
+     * Setter for camp visibility
+     * @param visibility The new visibility for the camp
+     */
     public void setVisibility(boolean visibility) {
         this.visibility = visibility;
     }
 
+
+    /**
+     * Getter for the camp faculty/ user group
+     * @return String of the faculty/ user group
+     */
     public String getUserGroup() {
         return this.userGroup;
     }
 
+
+    /**
+     * Setter for camp faculty/ user group
+     * @param userGroup The new faculty/ user group
+     */
     public void setUserGroup(String userGroup) {
         this.userGroup = userGroup;
     }
 
+
+    /**
+     * Getter for camp location
+     * @return String of camp location
+     */
     public String getLocation() {
         return this.location;
     }
 
+
+    /**
+     * Setter for camp location
+     * @param location The new location for the camp
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+
+    /**
+     * Getter for max registerable slots for the camp
+     * @return int of the max registetable slots
+     */
     public int getTotalSlots() {
         return this.totalSlots;
     }
 
+
+    /**
+     * Setter for the max registerable slots for the camp
+     * @param totalSlots The new max registerable slot
+     */
     public void setTotalSlots(int totalSlots) {
         this.totalSlots = totalSlots;
     }
 
+
+    /**
+     * Getter for the max committee slots
+     * @return int of the max committee slots
+     */
     public int getCommitteeSlots() {
         return this.committeeSlots;
     }
 
+
+    /**
+     * Setter for the max committee slots
+     * @param committeeSlots The new max committee slots
+     */
     public void setCommitteeSlots(int committeeSlots) {
         this.committeeSlots = committeeSlots;
     }
 
+
+    /**
+     * Getter for camp description
+     * @return String of the camp description
+     */
     public String getDescription() {
         return this.description;
     }
 
+
+    /**
+     * Setter for camp description
+     * @param description The new camp description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+
+    /**
+     * Getter for staff in charge name
+     * @return String of the staff in charge
+     */
     public String getStaffInCharge() {
         return this.staffInCharge;
     }
 
+
+    /**
+     * Getter for the names of the students registered
+     * @return ArrayList<String> of student names
+     */
     public ArrayList<String> getParticipatingStudentNames() {
         return this.participatingStudentNames;
     }
 
+
+    /**
+     * Getter for the names of the students who have withdrawn
+     * @return ArrayList<String> of student names
+     */
     public ArrayList<String> getWithdrawnStudentNames() {
         return this.withdrawnStudentNames;
     }
 
+
+    /**
+     * Getter for names of committee members
+     * @return ArrayList<String> of the committee member names
+     */
     public ArrayList<String> getCommitteeMemberNames() {
         return this.committeeMemberNames;
     }
 
+
+    /**
+     * Getter for enquiry IDs for the camp
+     * @return ArrayList<Integer> of enquiry IDs
+     */
     public ArrayList<Integer> getEnquiryIDs() {
         return this.enquiryIDs;
     }
 
+
+    /**
+     * Getter for suggestion IDs for the camp
+     * @return ArrayList<Integer> of suggestion IDs
+     */
     public ArrayList<Integer> getSuggestionIDs() {
         return this.suggestionIDs;
     }
--- cams/model/Enquiry.java
+++ cams/model/Enquiry.java
@@ -2,6 +2,10 @@ package cams.model;
 
 import java.io.Serializable;
 
+
+/**
+ * A model object for all relevant enquiry information
+ */
 public class Enquiry implements Serializable {
     private int id;
     private int campID;
@@ -12,6 +16,15 @@ public class Enquiry implements Serializable {
     private Boolean isResolved;
     private String resolvedBy;
 
+
+    /**
+     * Constructor for a new enquiry
+     * @param id The ID of the enquiry
+     * @param studentName The name of the student making the enquiry
+     * @param campID The ID of the camp the enquiry is for
+     * @param title The title of the enquiry
+     * @param content The enquiry content
+     */
     public Enquiry(int id, String studentName, int campID, String title, String content) {
 
         this.id = id;
@@ -24,6 +37,11 @@ public class Enquiry implements Serializable {
         this.resolvedBy = null;
     }
 
+
+    /**
+     * A method to make a copy of the enquiry object
+     * @param other The enquiry object to be copied
+     */
     public Enquiry(Enquiry other) {
         this.id = other.id;
         this.campID = other.campID;
@@ -35,66 +53,145 @@ public class Enquiry implements Serializable {
         this.resolvedBy = other.resolvedBy;
     }
 
+
+    /**
+     * Getter for the enquiry ID
+     * @return int of the enquiry ID
+     */
     public int getId() {
         return this.id;
     }
 
+
+    /**
+     * Setter for enquiry ID
+     * @param id The new enquiry ID
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Getter for camp ID enquiry belongs to
+     * @return int of camp ID
+     */
     public int getCampID() {
         return this.campID;
     }
 
+
+    /**
+     * Setter for camp ID enquiry belongs to
+     * @param campID The new camp ID
+     */
     public void setCampID(int campID) {
         this.campID = campID;
     }
 
+
+    /**
+     * Getter for the name of the student that wrote the enquiry
+     * @return String of the student name
+     */
     public String getStudentName() {
         return this.studentName;
     }
 
+
+    /**
+     * Setter for the name of student that wrote the enquiry
+     * @param studentName The new student name
+     */
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
 
+
+    /**
+     * Getter for the title
+     * @return String of the title
+     */
     public String getTitle() {
         return this.title;
     }
 
+    
+    /**
+     * Setter for the title
+     * @param title The new title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+
+    /**
+     * Getter for the enquiry content
+     * @return String of the enquiry content
+     */
     public String getContent() {
         return this.content;
     }
 
+
+    /**
+     * Setter for enquiry content
+     * @param content The new enquiry content
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+
+    /**
+     * Getter for the reply of the enquiry
+     * @return String of the reply
+     */
     public String getReply() {
         return this.reply;
     }
 
+
+    /**
+     * Setter for reply for the enquiry
+     * @param reply The new reply
+     */
     public void setReply(String reply) {
         this.reply = reply;
     }
 
+
+    /**
+     * Getter for whether enquiry is resolved
+     * @return boolean of whether enquiry is resolved
+     */
     public Boolean getIsResolved() {
         return this.isResolved;
     }
 
+
+    /**
+     * Setter for whether enquiry is resolved
+     * @param isResolved The new resolved state
+     */
     public void setIsResolved(boolean isResolved) {
         this.isResolved = isResolved;
     }
 
+
+    /**
+     * Getter for the name of user who resolved enquiry
+     * @return String of user name
+     */
     public String getResolvedBy() {
         return this.resolvedBy;
     }
 
+
+    /**
+     * Setter for name of user who resolved the enquiry
+     * @param userName The resolver's name
+     */
     public void setResolvedBy(String userName) {
         this.resolvedBy = userName;
     }
